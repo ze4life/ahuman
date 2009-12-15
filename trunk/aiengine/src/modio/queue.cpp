889f6c69a5ae95878788d554cc6c45b7a70a16fb
sarbjit.longia <sarbjit.longia@gmail.com>
2009-12-15 06:25:48 +0000

    Added the comments and also the description of each API.

--- trunk/aiengine/src/modio/queue.cpp
+++ trunk/aiengine/src/modio/queue.cpp
@@ -1,24 +1,35 @@
+/*#########################################################################
+ * This class is the implementation of the queue data structure so that all
+ * the messages from various sources can be added and processed later.
+/*#########################################################################*/
 
 #include "aiio_impl.h"
 
+/* Define the name of the queue */
 const char *IOQueue::NAME = "IOQueue";
 
-/*#########################################################################*/
+/*#########################################################################
 /*#########################################################################*/
 
-// sync queue
-// class IOQueue 
+/*
+ * Constructer to create the instance of queue so that we can 
+ * create a queue to append messages to.
+ * @param queue ID
+ */
 IOQueue::IOQueue( String p_queueId )
 :	engine( AIEngine::getInstance() )
 {
 	queueId = p_queueId;
 	queueLock = rfc_lock_create();
 	queueWakeupEvent = rfc_hnd_evcreate();
+	/* Create a list to add messages of type pointer */
 	queueMessages = rfc_lst_create( RFC_EXT_TYPEPTR );
-
 	logger.attach( this );
 }
 
+/*
+ * Destructor that clears all the messages from queue.
+ */
 IOQueue::~IOQueue()
 {
 	// delete all pending messages
@@ -29,6 +40,10 @@ IOQueue::~IOQueue()
 	rfc_lock_destroy( queueLock );
 }
 
+/*
+ * This function adds the message passed, to the queue.
+ * @param Message Pointer
+ */
 void IOQueue::addMessage( Message *p_str )
 {
 	rfc_lock_exclusive( queueLock );
@@ -47,6 +62,9 @@ void IOQueue::addMessage( Message *p_str )
 	rfc_lock_release( queueLock );
 }	
 
+/*
+ * Gets the message from top of the queue.
+ */
 Message *IOQueue::getNextMessage()
 {
 	rfc_lock_exclusive( queueLock );
@@ -74,6 +92,7 @@ Message *IOQueue::getNextMessage()
 
 	// get first item
 	RFC_TYPE *val = rfc_lst_get( queueMessages , 0 );
+	/* Get u_p type as the list that we created was of type pointer */
 	Message *x = ( Message * )val -> u_p;
 
 	// remove from queue
@@ -116,6 +135,9 @@ Message *IOQueue::getNextMessageNoLock()
 	return( x );
 }
 
+/*
+ * This API will clear all the messages and will send the wake up event
+ */
 void IOQueue::makeEmptyAndWakeup()
 {
 	rfc_lock_exclusive( queueLock );
@@ -129,6 +151,9 @@ void IOQueue::makeEmptyAndWakeup()
 	rfc_lock_release( queueLock );
 }
 
+/*
+ * Check to ses if the list has some messages or not.
+ */
 bool IOQueue::isEmpty()
 {
 	rfc_lock_shared( queueLock );
@@ -139,6 +164,9 @@ bool IOQueue::isEmpty()
 	return( retval );
 }
 
+/*
+ * Delete all the messages from queue.
+ */
 void IOQueue::clearMessages()
 {
 	for( int k = rfc_lst_count( queueMessages ) - 1; k >= 0; k-- ) 
