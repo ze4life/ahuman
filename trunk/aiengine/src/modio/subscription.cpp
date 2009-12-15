2311c52725e777354ec893ab9063234c68518fec
sarbjit.longia@gmail.com <sarbjit.longia@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2009-12-15 06:56:53 +0000

    Added comments.

--- trunk/aiengine/src/modio/subscription.cpp
+++ trunk/aiengine/src/modio/subscription.cpp
@@ -1,8 +1,19 @@
+/*#########################################################################
+ * This class is responsible to process the message depending upon the
+ * type of message.
+/*#########################################################################*/
 #include "aiio_impl.h"
 
 /*#########################################################################*/
 /*#########################################################################*/
 
+/*
+ * Constructor to initialize the instance with the required parameters.
+ * @param p_session is the pointer to session.
+ * @param p_channel is the channel instance
+ * @param p_name is the subscription name
+ * @param p_sub is the subscriber
+ */
 SubscriptionImpl::SubscriptionImpl( Session *p_session , Channel *p_channel , String p_name , Subscriber *p_sub )
 {
 	session = p_session;
@@ -11,16 +22,28 @@ SubscriptionImpl::SubscriptionImpl( Session *p_session , Channel *p_channel , St
 	sub = p_sub;
 }
 
+/*
+ * Returns the channel.
+ */
 Channel *SubscriptionImpl::getChannel()
 {
 	return( channel );
 }
 
+/*
+ * Disconnects the channel.
+ */
 void SubscriptionImpl::disconnected()
 {
+    /* TODO; need to see if the memory is free after channel pointer is
+        no longer required */
 	channel = NULL;
 }
 
+/*
+ * Process the message deepnding upon the type.
+ * @param msg is the message to be processed.
+ */
 void SubscriptionImpl::processMessage( Message *msg )
 {
 	switch( msg -> getMsgBaseType() )
