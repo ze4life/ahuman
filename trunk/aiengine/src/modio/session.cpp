78cf7e3fceeccf51e7793ee7d45c88dcd628c6a8
sarbjit.longia@gmail.com <sarbjit.longia@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2009-12-15 06:33:53 +0000

    Updated with the comments and file headers.

--- trunk/aiengine/src/modio/session.cpp
+++ trunk/aiengine/src/modio/session.cpp
@@ -1,25 +1,46 @@
+/*#########################################################################
+ * This class is takes care of the session. The sessions are stored in the 
+ * hashmap kind of datastructure.
+/*#########################################################################*/
 #include "aiio_impl.h"
 
 /*#########################################################################*/
 /*#########################################################################*/
 
+/*
+ * This is the constructor that will initialise the session object with
+ * the passed ID.
+ * @param Session ID
+ */
 SessionImpl::SessionImpl( int id )
 {
 	sessionId = id;
 	dataLock = rfc_lock_create();
 }
 
+/*
+ * Destructor that will destroy any mapping between objects and
+ * session names.
+ */
 SessionImpl::~SessionImpl()
 {
 	rfc_lock_destroy( dataLock );
 	objects.destroy();
 }
 
+/*
+ * Returns the session ID
+ */
 int SessionImpl::getSessionId()
 {
 	return( sessionId );
 }
 
+/*
+ * Adds the mapping between the object and the name.
+ * @param o is the session object
+ * @param name is the session name
+ */
 void SessionImpl::setObject( Object *o , String name )
 {
 	rfc_lock_exclusive( dataLock );
@@ -30,6 +51,10 @@ void SessionImpl::setObject( Object *o , String name )
 	ASSERT( oOld == NULL );
 }
 
+/*
+ * Returns the session given the name.
+ * @param name is the session name.
+ */
 Object *SessionImpl::getObject( String name )
 {
 	rfc_lock_shared( dataLock );
