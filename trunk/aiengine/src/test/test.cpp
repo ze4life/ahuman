8e66a1df1648b38ca6f3a71051d22040780082d4
vsavchik <vsavchik@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2010-06-27 11:37:44 +0000

    restructure by multi-lib

---
+++ trunk/aiengine/src/test/test.cpp
@@ -0,0 +1,107 @@
+#include "test_impl.h"
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+AITestPool::AITestPool() { 
+	thisPtr = AITestPoolImpl::getServiceImpl(); 
+}
+
+AITestPoolImpl *AITestPoolImpl::getServiceImpl() {
+	return( static_cast<AITestPoolImpl *>( AIEngine::getInstance().getService( "TestPool" ) ) );
+}
+
+/* static */ Service *AITestPool::createService() {
+	Service *svc = new AITestPoolImpl();
+	AIEngine::getInstance().registerService( svc , "TestPool" );
+	return( svc );
+}
+
+AITestPoolImpl::AITestPoolImpl()
+:	engine( AIEngine::getInstance() ) {
+}
+
+void AITestPoolImpl::initService() {
+	// create units
+	addTestUnit( TestUnit::createFannCustom() );
+	addTestUnit( TestUnit::createHtmViewCustom() );
+	
+	// log available commands
+	logger.logInfo( "AVAILABLE TEST METHODS:" );
+	for( int k = 0; k < units.count(); k++ ) {
+		TestUnit *tu = units.getClassByIndex( k );
+		
+		MapStringToClass<TestMethod>& methods = tu -> methods;
+		for( int m = 0; m < methods.count(); m++ ) {
+			logger.logInfo( String( "method=" ) + methods.getKeyByIndex( m ) );
+		}
+	}
+}
+
+void AITestPoolImpl::runService() {
+	// subscribe
+	Xml xml = Service::config.getFirstChild( "channels" );
+	String channelIn = xml.getProperty( "request" );
+	String channelOut = xml.getProperty( "response" );
+
+	AIIO io;
+	callSub = io.subscribe( NULL , channelIn , "testpool" , this );
+	callPub = io.createPublisher( NULL , channelOut , "testpool" , "test" );
+}
+
+void AITestPoolImpl::exitService() {
+	AIIO io;
+	if( callPub != NULL )
+		io.destroyPublisher( callPub );
+	if( callSub != NULL )
+		io.unsubscribe( callSub );
+		
+	units.destroy();
+}
+
+void AITestPoolImpl::destroyService() {
+	delete this;
+}
+
+void AITestPoolImpl::addTestUnit( TestUnit *p_unit ) {
+	units.add( p_unit -> getName() , p_unit );
+	p_unit -> init();
+}
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+void AITestPoolImpl::onXmlCall( XmlCall *msg ) {
+	XmlCall& call = *msg;
+	String cn = call.getClassName();
+	String fn = call.getFunctionName();
+	String fnfull = cn + "::" + fn;
+	logger.logInfo( fnfull + " called in session " + call.getSession() -> getSessionId() );
+
+	// find unit
+	TestUnit *unit = units.get( cn );
+	if( unit == NULL )
+		return;
+		
+	// find method
+	TestMethod *method = unit -> getMethod( fnfull );
+	if( method == NULL )
+		return;
+
+	try {
+		logger.logInfo( fnfull + " method found in test classes" );
+		unit -> logger.attach( fnfull );
+		unit -> callMethod( method , call );
+		call.sendStatusResponse( "OK" );
+	}
+	catch ( RuntimeException& e ) {
+		call.sendResponseException( callPub , e );
+		e.printStack( logger );
+	}
+	catch( ... ) {
+		call.sendResponseUnknownException( callPub );
+		logger.logError( "Unknown exception:" );
+		logger.printStack();
+	}
+}
+
---
+++ trunk/aiengine/src/test/test_impl.h
@@ -0,0 +1,113 @@
+#include <aiengine.h>
+#include <aiio.h>
+#include <test.h>
+
+// core classes
+class TestUnit;
+class TestMethod;
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+// derives knowledge from io, activates mind
+class AITestPoolImpl : public AITestPool , public Service , public Subscriber
+{
+public:
+	// service
+	virtual void initService();
+	virtual void runService();
+	virtual void exitService();
+	virtual void destroyService();
+	virtual const char *getName() { return( "TestPool" ); };
+
+	// subscriber
+	virtual void onXmlCall( XmlCall *msg );
+
+// external interface
+public:
+	AITestPoolImpl();
+	static AITestPoolImpl *getServiceImpl();
+
+// internals
+private:
+	void addTestUnit( TestUnit *p_unit );
+
+private:
+	AIEngine& engine;
+
+	Publisher *callPub;
+	Subscription *callSub;
+	MapStringToClass<TestUnit> units;
+};
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+typedef void ( TestUnit::*TestUnitMethodType )( XmlCall& );
+#define ADD_METHOD( method ) TestUnit::addMethod( #method , ( TestUnitMethodType )&method )
+
+class TestMethod
+{
+public:
+	TestMethod( String p_name , TestUnitMethodType p_fptr ) {
+		name = p_name;
+		fptr = p_fptr;
+	}
+	
+	String name;
+	TestUnitMethodType fptr;
+};
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+class TestUnit
+{
+// test units
+public:
+	static TestUnit *createFannCustom();
+	static TestUnit *createHtmViewCustom();
+
+// generic
+public:
+	TestUnit( String p_name ) {
+		name = p_name;
+	}
+	~TestUnit() {
+		methods.destroy();
+	}
+
+	String getName() {
+		return( name );
+	}
+
+	void create() {
+		logger.attach( name );
+		init();
+	}
+
+	TestMethod *getMethod( String methodName ) {
+		return( methods.get( methodName ) );
+	}
+	
+	void callMethod( TestMethod *method , XmlCall& call ) {
+		( *this.*(method->fptr) )( call );
+	}
+	
+	virtual void init() = 0;
+	
+protected:
+	void addMethod( String methodName , TestUnitMethodType methodFunction ) {
+		methods.add( methodName , new TestMethod( methodName , methodFunction ) );
+	}
+
+// virtuals
+public:
+	Logger logger;
+	
+	String name;
+	MapStringToClass<TestMethod> methods;
+};
+
+/*#########################################################################*/
+/*#########################################################################*/
