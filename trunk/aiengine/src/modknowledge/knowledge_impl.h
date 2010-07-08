b1722b16b30fdb90d59d3cb2ffe8c375816a9d30
vsavchik <vsavchik@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2010-07-08 18:39:02 +0000

    change structure to architecture

---
+++ trunk/aiengine/src/modknowledge/knowledge_impl.h
@@ -0,0 +1,36 @@
+#ifndef	INCLUDE_AIKNOWLEDGE_IMPL_H
+#define INCLUDE_AIKNOWLEDGE_IMPL_H
+
+#include <aiengine.h>
+#include <aiknowledge.h>
+#include <aisvcdb.h>
+#include <aisvcio.h>
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+// derives knowledge from io, activates mind
+class AIKnowledgeImpl : public AIKnowledge , public Service
+{
+	// service
+	virtual void initService();
+	virtual void runService();
+	virtual void exitService();
+	virtual void destroyService();
+	virtual const char *getName() { return( "Knowledge" ); };
+
+// external interface
+public:
+	AIKnowledgeImpl();
+
+	// Object interface
+
+// internals
+private:
+	AIEngine& engine;
+};
+
+// #############################################################################
+// #############################################################################
+
+#endif	// INCLUDE_AIKNOWLEDGE_IMPL_H
