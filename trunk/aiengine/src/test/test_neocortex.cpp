cd637d98476caa2d21466fdbe2d7a68be1a390ba
sarbjit.longia@gmail.com <sarbjit.longia@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2010-10-08 14:39:41 +0000

    Updated test case with new code for Sf neocortex

--- trunk/aiengine/src/test/test_neocortex.cpp
+++ trunk/aiengine/src/test/test_neocortex.cpp
@@ -25,8 +25,7 @@ public:
 	void testCreateSimple( XmlCall& call ) {
 		// use libbn sf_neocortex library directly without unified interface
 		// set parameters
-		SFNeoCortex ct( AIBrain::getNeoCortexArea() , 32 );
-		ct.areaSide = 32;
+		SFNeoCortex ct( AIBrain::getNeoCortexArea() , 32, 32 );
 		ct.bestMatchPrecision = 0.95;
 		ct.overlapSubRegions = 0;
 		// Delete a Percentage of memories/Delete memories below threshold
@@ -34,44 +33,20 @@ public:
 		ct.predictionCount = 10;
 		//derived: whole number of overlapping subregions that fit into NeoParameters::ImageSide
 		//image side covered by hierarchy. May not cover whole bitmap due to overlap
-		ct.regionCount = 3;
+		ct.setRegionCount(3);
 		ct.regionForgetThreshold.add( 0.07 ); ct.regionForgetThreshold.add( 3.5 ); ct.regionForgetThreshold.add( 0.25 );
 		ct.regionMemorySize.add( 3 , 30000 );
 		ct.regionLowUsageThreshold.add( 3 , 2 );
 		ct.regionSideCompression.add( 4 ); ct.regionSideCompression.add( 2 ); ct.regionSideCompression.add( 2 );
 		ct.sequenceLength.add( 3 , 1 );
 		// derived: whole number of overlapping subregions that fit into ImageSide
-		double lSide = ( double(ct.areaSide - ct.overlapSubRegions ) / double(ct.regionSideCompression[0] - ct.overlapSubRegions ) );
-		ct.bottomRegionSide = ( unsigned )lSide;
+		//double lSide = ( double(ct.areaSide - ct.overlapSubRegions ) / double(ct.regionSideCompression[0] - ct.overlapSubRegions ) );
+		//ct.bottomRegionSide = ( unsigned )lSide;
 
 		// create cortex
 		BitmapVision eye( ct );
-
-		ClassList<NeoRegion> regions;
-		unsigned i, side = 0;
-		for(i = 0; i < ct.regionCount; i++) {
-			if(i == 0)
-				side = ct.bottomRegionSide;
-			else
-				side /= ct.regionSideCompression[i];
-
-			NeoRegion *region = new NeoRegion(ct, side, side, ct.sequenceLength[i], ct.regionSideCompression[i], i);
-			regions.add( region );
-
-			if(i > 0)
-				regions[i] -> setChild(regions[i-1]);
-			cInitForInferenceDone = false;
-		}
-
-		for(i = 0; i < ct.regionCount-1; i++)
-			regions[i] -> setParent( regions[i+1] );
-
-		regions[0] -> setChild( &eye );
-		eye.setParent( regions[0] );
-
-		Hippocampus hippo(ct, side); //side of top region == side compression of hippocampus to give '1'
-		hippo.setChild( regions[ct.regionCount-1] );
-		regions[ct.regionCount-1] -> setParent( &hippo );
+		ct.createCortexNetwork();
+		ct.setSense(&eye);
 	}
 };
 
