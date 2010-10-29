e9ac66939fe69dfd954d613575cd8b3363e9c9f3
sarbjit.longia@gmail.com <sarbjit.longia@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2010-10-29 18:06:49 +0000

    Added eye sensor, need to implement this class using different interfaces including image KB

---
+++ trunk/aiengine/src/modbody/sensors/eye.cpp
@@ -0,0 +1,43 @@
+#include "../body_impl.h"
+#include <windows.h>
+
+class Eye : public Sensor, public Object{
+
+public:
+	AIEngine& engine;
+
+	Eye( MindArea *area ) :	Sensor( "EyeSensor" , area , CortexIOSizeInfo( 20 , 2 ) ) ,
+		engine( AIEngine::getInstance() ) {
+
+	}
+
+	virtual ~Eye() {
+
+	}
+
+	virtual const char *getClass() {
+		return( "Eye" ); 
+	}
+
+	virtual void startSensor(){
+
+	}
+	virtual void stopSensor(){
+
+	}
+	virtual void processSensorControl(){
+
+	}
+	virtual bool executeSensorControl(){
+		return true;
+	}
+	virtual void produceSensorData() {
+
+	}
+
+};
+
+Sensor *Sensor::createEye( MindArea *area ){
+	Eye *att = new Eye( area );
+	return( att );
+}
