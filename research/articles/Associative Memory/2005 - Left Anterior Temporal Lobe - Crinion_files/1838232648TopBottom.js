da604aeba3462fb84801ade5cee829e1dd911ab0
vsavchik <vsavchik@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2011-03-22 10:28:29 +0000

    sync

---
+++ research/articles/Associative Memory/2005 - Left Anterior Temporal Lobe - Crinion_files/1838232648TopBottom.js
@@ -0,0 +1,8 @@
+function OAS_RICH(position) {
+if (position == 'Bottom') {
+document.write ('<A HREF="http://oas.oxfordjournals.org/5c/cercor.oxfordjournals.org/cgi/content/full/16/8/1116/240172281/Bottom/default/empty.gif/562b52375355774e5076304143335949?x" target="_top"><IMG SRC="http://imagec16.247realmedia.com/0/default/empty.gif"  WIDTH=1 HEIGHT=1 ALT="" BORDER=0 BORDER="0"></A>');
+}
+if (position == 'Top') {
+document.write ('<A HREF="http://oas.oxfordjournals.org/5c/cercor.oxfordjournals.org/cgi/content/full/16/8/1116/1047280538/Top/default/empty.gif/562b52375355774e5076304143335949?x" target="_top"><IMG SRC="http://imagec16.247realmedia.com/0/default/empty.gif"  WIDTH=1 HEIGHT=1 ALT="" BORDER=0 BORDER="0"></A>');
+}
+}
