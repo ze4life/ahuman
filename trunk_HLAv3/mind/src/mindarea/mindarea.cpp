95bf157d3b781f9ed15d853869dbdedf4f085be1
vsavchik <vsavchik@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2011-05-12 18:46:08 +0000

    dirs

---
+++ trunk_HLAv3/mind/src/mindarea/mindarea.cpp
@@ -0,0 +1,37 @@
+#include <ah_mind.h>
+#include <ah_mind_impl.h>
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+MindArea::MindArea() {
+	info = NULL;
+	regionSet = NULL;
+	regionLinkSet = NULL;
+}
+
+MindArea::~MindArea() {
+}
+
+void MindArea::configureArea( MindAreaInfo *p_info ) {
+	info = p_info;
+}
+
+void MindArea::createArea() {
+	regionSet = new MindRegionSet;
+	regionLinkSet = new MindRegionLinkSet;
+}
+
+void MindArea::exitArea() {
+	regionSet -> exitRegionSet();
+	regionLinkSet -> exitRegionLinkSet();
+}
+
+void MindArea::destroyArea() {
+	regionSet -> destroyRegionSet();
+	regionLinkSet -> destroyRegionLinkSet();
+	delete regionSet;
+	regionSet = NULL;
+	delete regionLinkSet;
+	regionLinkSet = NULL;
+}
