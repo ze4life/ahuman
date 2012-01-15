6375447f543e2ab209c003bf7d79daeea616261f
vsavchik <vsavchik@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2012-01-15 15:39:30 +0000

    add scheduler service

--- trunk_HLAv3/platform/src/scheduler/schedulertask.cpp
+++ trunk_HLAv3/platform/src/scheduler/schedulertask.cpp
@@ -33,7 +33,7 @@ void SchedulerTask::start( RFC_HND stopEvent ) {
 	ASSERTMSG( taskThread == ( RFC_HND )NULL , "thread is running for task name=" + name );
 	ThreadService *ts = ThreadService::getService();
 	stopSignal = false;
-	taskThread = ts -> runThread( "scheduler." + name , this , ( ObjectThreadFunction )&SchedulerTask::runThreadTask , NULL );
+	taskThread = ts -> runThread( "scheduler." + name , this , ( ObjectThreadFunction )&SchedulerTask::runSchedulerTask , NULL );
 }
 
 void SchedulerTask::stop() {
@@ -45,8 +45,34 @@ void SchedulerTask::stop() {
 	ts -> waitThreadExited( taskThread );
 }
 
-void SchedulerTask::runThreadTask( void *p_arg ) {
-	TextMessage *msg = notifyPublisher -> createTextMessage();
-	msg -> setProperty( "TASK" , name );
-	notifyPublisher -> publish( NULL , msg );
+void SchedulerTask::runSchedulerTask( void *p_arg ) {
+	logger.logInfo( "runSchedulerTask: started task name=" + name );
+
+	RFC_INT64 startTime = Timer::getCurrentTimeMillis();
+	RFC_INT64 lastTime = startTime;
+	RFC_INT64 interval = intervalSec * 1000;
+
+	while( !stopSignal ) {
+		// keep interval
+		RFC_INT64 nextTime = lastTime + interval;
+		RFC_INT64 currentTime = Timer::getCurrentTimeMillis();
+
+		// sleep to keep interval
+		if( nextTime > currentTime )
+			rfc_hnd_waitevent( stopEvent , ( int )( nextTime - currentTime ) );
+
+		// check sleep ended by stop signal
+		if( stopSignal )
+			break;
+
+		// execute task		
+		logger.logInfo( "runSchedulerTask: execute task name=" + name );
+		TextMessage *msg = notifyPublisher -> createTextMessage();
+		msg -> setProperty( "TASK" , name );
+		notifyPublisher -> publish( NULL , msg );
+
+		lastTime = nextTime;
+	}
+
+	logger.logInfo( "runSchedulerTask: stopped task name=" + name );
 }
