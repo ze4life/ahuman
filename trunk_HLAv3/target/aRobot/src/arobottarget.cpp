e81ac0f46b2361d804b40277251492b2b6f83208
vsavchik <vsavchik@gmail.com>
2013-03-09 17:59:54 +0000

    delete wrong

--- trunk_HLAv3/target/aRobot/src/arobottarget.cpp
+++ trunk_HLAv3/target/aRobot/src/arobottarget.cpp
@@ -4,12 +4,15 @@
 /*#########################################################################*/
 
 ARobotTarget::ARobotTarget() {
+	attachLogger();
 }
 
 void ARobotTarget::configureTarget( Xml config ) {
 }
 
 void ARobotTarget::createTarget( SensorArea *sensorArea , EffectorArea *effectorArea ) {
+	ASSERTMSG( nxt.create() , "unable to initiate connection to aRobot" );
+
 	MindTarget::addSensor( ARobotTarget::createEye( sensorArea ) );
 	MindTarget::addEffector( ARobotTarget::createWheeledLegs( effectorArea ) );
 }
@@ -21,9 +24,30 @@ void ARobotTarget::initEffectorsTarget( EffectorArea *effectorArea ) {
 }
 
 void ARobotTarget::runTarget() {
+	Timer t;
+	t.createRunWindowSec( 5 );
+
+	for( int k = 0; k < 10; k++ ) {
+		logger.logInfo( "turn forward 20 both" );
+		nxt.motorSetPower( 2 , 20 );
+		nxt.motorSetPower( 3 , 20 );
+		logger.logInfo( "sleep" );
+
+		t.waitNext();
+
+		logger.logInfo( "turn backward 20 both" );
+		nxt.motorSetPower( 2 , -20 );
+		nxt.motorSetPower( 3 , -20 );
+		logger.logInfo( "sleep" );
+
+		t.waitNext();
+	}
+
+	nxt.destroy();
 }
 
 void ARobotTarget::stopTarget() {
+	nxt.destroy();
 }
 
 void ARobotTarget::exitTarget() {
