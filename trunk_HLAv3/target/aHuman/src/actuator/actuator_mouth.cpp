bbfa044de29ae485450144ea6e69a4b6c0cfa920
vsavchik@gmail.com <vsavchik@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2013-07-18 20:50:47 +0000

    add mouth

---
+++ trunk_HLAv3/target/aHuman/src/actuator/actuator_mouth.cpp
@@ -0,0 +1,87 @@
+#include "stdafx.h"
+#include "ahumantarget.h"
+
+// #############################################################################
+// #############################################################################
+
+class EffectorMouth : public MindEffector {
+private:
+	bool continueRunFlag;
+
+public:
+	EffectorMouth( EffectorArea *area );
+	virtual ~EffectorMouth();
+	virtual const char *getClass() { return( "EffectorMouth" ); };
+
+public:
+	// effector lifecycle
+	virtual void createEffector( MindRegionCreateInfo *createInfo );
+	virtual void configureEffector( Xml config );
+	virtual void startEffector() {};
+	virtual void stopEffector() {};
+	virtual void processEffectorControl( NeuroLink *link , NeuroSignal *signal ) {};
+
+private:
+	void createSourceConnector( MindRegionCreateInfo *createInfo , MindRegionConnectorDef *connector );
+	void createTargetConnector( MindRegionCreateInfo *createInfo , MindRegionConnectorDef *connector );
+
+	NeuroSignal *sourceHandler( NeuroLink *link , NeuroLinkSource *point );
+	NeuroSignalSet *targetHandler( NeuroLink *link , NeuroLinkTarget *point , NeuroSignal *srcData );
+
+private:
+	Xml config;
+};
+
+MindEffector *AHumanTarget::createMouth( EffectorArea *area ) {
+	return( new EffectorMouth( area ) );
+}
+
+// #############################################################################
+// #############################################################################
+
+EffectorMouth::EffectorMouth( EffectorArea *p_area )
+:	MindEffector( p_area ) {
+	attachLogger();
+	continueRunFlag = false;
+}
+
+EffectorMouth::~EffectorMouth() {
+}
+
+void EffectorMouth::configureEffector( Xml p_config ) {
+	config = p_config;
+}
+
+void EffectorMouth::createEffector( MindRegionCreateInfo *createInfo ) {
+	// set connectors
+	TargetRegionDef *info = MindEffector::getEffectorInfo();
+	MindRegionTypeDef *type = info -> getType();
+	ClassList<MindRegionConnectorDef>& connectors = type -> getConnectors();
+	for( int k = 0; k < connectors.count(); k++ ) {
+		MindRegionConnectorDef *connector = connectors.get( k );
+		if( connector -> isTarget() )
+			createTargetConnector( createInfo , connector );
+		else
+			createSourceConnector( createInfo , connector );
+	}
+}
+
+void EffectorMouth::createSourceConnector( MindRegionCreateInfo *createInfo , MindRegionConnectorDef *connector ) {
+	NeuroLinkSource *source = new NeuroLinkSource();
+	source -> setHandler( ( MindRegion::NeuroLinkSourceHandler )&EffectorMouth::sourceHandler );
+	MindEffector::addSourceEntity( connector -> getId() , source );
+}
+
+void EffectorMouth::createTargetConnector( MindRegionCreateInfo *createInfo , MindRegionConnectorDef *connector ) {
+	NeuroLinkTarget *target = new NeuroLinkTarget();
+	target -> setHandler( ( MindRegion::NeuroLinkTargetHandler )&EffectorMouth::targetHandler );
+	MindEffector::addTargetEntity( connector -> getId() , target );
+}
+
+NeuroSignal *EffectorMouth::sourceHandler( NeuroLink *link , NeuroLinkSource *point ) {
+	return( NULL );
+}
+
+NeuroSignalSet *EffectorMouth::targetHandler( NeuroLink *link , NeuroLinkTarget *point , NeuroSignal *srcData ) {
+	return( NULL );
+}
