8316c143ad0392cb2e1bd4f3d716206587d9ab92
vsavchik@gmail.com <vsavchik@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2014-01-05 20:07:00 +0000

    add effectors

---
+++ trunk_HLAv3/target/aHuman/src/wikimusclespecpages.cpp
@@ -0,0 +1,75 @@
+#include "stdafx.h"
+#include "ahumantarget.h"
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+WikiMuscleSpecPages::WikiMuscleSpecPages( WikiMaker *p_wm ) {
+	attachLogger();
+	wm = p_wm;
+}
+
+WikiMuscleSpecPages::~WikiMuscleSpecPages() {
+}
+
+void WikiMuscleSpecPages::execute() {
+	bool createMainPage = wm -> wiki.getBooleanProperty( "createMusclePages" , true );
+	if( createMainPage == false ) {
+		logger.logInfo( "skip creating muscle spec pages" );
+		return;
+	}
+
+	// get nerves from pns
+	MapStringToClass<XmlMuscleDivision>& divs = wm -> musclesxml.getDivisions();
+
+	// collect model circuit section lines
+	for( int k = 0; k < divs.count(); k++ )
+		addMuscleDivision( divs.getClassRefByIndex( k ) );
+}
+
+void WikiMuscleSpecPages::addMuscleDivision( XmlMuscleDivision& div ) {
+	StringList lines;
+	String divPage = div.page;
+	String divSection = div.name;
+
+	for( int k = 0; k < div.childs.count(); k++ )
+		addMuscleList( 0 , div.childs.getClassRefByIndex( k ) , lines );
+
+	String wikiDir = wm -> wiki.getProperty( "wikiPath" );
+	wm -> updateFileSection( wikiDir , divPage , divSection , lines );
+}
+
+void WikiMuscleSpecPages::addMuscleList( int level , XmlMuscleInfo& muscle , StringList& lines ) {
+	// item string
+	String name;
+	if( muscle.link.isEmpty() )
+		name = muscle.name;
+	else
+		name = "[" + muscle.link + " " + muscle.name + "]";
+	String s = String( " " ).replicate( level + 2 ) + "* *" + name + "*";
+
+	if( !muscle.nerve.isEmpty() ) {
+		String page = wm -> getNerveWikiPage( muscle.nerve );
+		String pagelink = "[" + page + " " + muscle.nerve + "]";
+		s += "; NERVE={" + pagelink + "}";
+	}
+	if( !muscle.nervelist.isEmpty() )
+		s += "; NERVELIST={" + muscle.nervelist + "}";
+	if( !muscle.action.isEmpty() )
+		s += "; ACTION={" + muscle.action + "}";
+	
+	lines.add( s );
+
+	// childs
+	for( int k = 0; k < muscle.childs.count(); k++ )
+		addMuscleList( level + 1 , muscle.childs.getClassRefByIndex( k ) , lines );
+
+	// image
+	if( !muscle.imgsrc.isEmpty() ) {
+		s = String( " " ).replicate( level + 3 ) + "* <img src=\"" + muscle.imgsrc + "\"";
+		if( !muscle.imgheight.isEmpty() )
+			s += " height=" + muscle.imgheight;
+		s += ">";
+		lines.add( s );
+	}
+}
