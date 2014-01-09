80b6fcdd4a2de46899d55d973a0f50dd081b85ed
vsavchik@gmail.com <vsavchik@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2014-01-09 21:13:06 +0000

    add info

---
+++ trunk_HLAv3/target/aHuman/include/ahumanverify.h
@@ -0,0 +1,73 @@
+#include "stdafx.h"
+#include "xmlhuman.h"
+#include "ahumanmodel.h"
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+class ModelVerifier;
+class ModelFiberValidator;
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+typedef enum {
+	FIBER_CHAIN_POS_BEGIN = 1 ,
+	FIBER_CHAIN_POS_END = 2 ,
+	FIBER_CHAIN_POS_MID = 3
+} ModelVerifierFiberChainPosEnum;
+
+class ModelVerifier : public MindModel {
+public:
+	ModelVerifier( Xml modelArea );
+	virtual ~ModelVerifier();
+	virtual const char *getClass() { return "ModelVerifier"; };
+
+public:
+	void verify();
+
+private:
+	void checkHierarchy();
+	void checkCircuits();
+	void checkNerves();
+	void checkMuscles();
+	void checkMindModel();
+
+	bool checkHierarchy_verifyChild( String node , bool checkMapping );
+	bool checkCircuits_verifyComponents( String circuit );
+	bool checkCircuits_verifyLinks( String circuit );
+	bool checkCircuits_verifyCircuitLink( XmlCircuitInfo& circuit , XmlCircuitLinkInfo& link );
+	bool checkNerves_verifyComponents( String nerve );
+	bool checkNerves_verifyLinks( XmlNerveInfo& info , XmlNerveFiberInfo& nf );
+	bool checkNerves_verifyFiberChain( XmlNerveInfo& info , XmlNerveFiberInfo& nf , String regionSrcId , String regionDstId , ModelVerifierFiberChainPosEnum pos );
+	bool checkMuscles_verifyNerves( String muscle );
+	bool checkFiberComp( XmlNerveInfo& info , XmlNerveFiberInfo& nf , String comp );
+	bool checkFiberType( XmlNerveInfo& info , XmlNerveFiberInfo& nf , String type );
+	bool checkMindModel_verifyRegion( MindRegionDef *regionDef );
+	bool checkMindModel_verifyLinkedConnectors( MindRegionDef *regionDef , MindRegion *region );
+	bool checkMindModel_verifyRegionCircuits( MindRegionDef *regionDef , MindRegion *region );
+
+private:
+	Xml modelArea;
+
+	MapStringToClass<MindRegionDef> regionMap;
+	MapStringToClass<MindRegionDef> hierarchyMap;
+};
+
+/*#########################################################################*/
+/*#########################################################################*/
+
+class ModelFiberValidator : public Object {
+public:
+	ModelFiberValidator();
+	virtual ~ModelFiberValidator();
+	virtual const char *getClass() { return "ModelFiberValidator"; };
+
+public:
+	bool isValid_GSE_end( XmlNerveInfo& info , MindRegion *src , MindRegion *dst );
+	bool isValid_GSA_begin( XmlNerveInfo& info , MindRegion *src , MindRegion *dst );
+	bool isValid_GSA_end( XmlNerveInfo& info , MindRegion *src , MindRegion *dst );
+};
+
+/*#########################################################################*/
+/*#########################################################################*/
