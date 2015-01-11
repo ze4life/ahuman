fc5ae8fbe1dc91c7b1a4f6e6878489eeb50d4ba7
vsavchik@gmail.com <vsavchik@gmail.com@d324336c-43d6-11de-a5f8-e1a32e2bd165>
2015-01-11 13:12:39 +0000

    rework

--- trunk_HLAv3/platform/src/threads/threaddata.cpp
+++ trunk_HLAv3/platform/src/threads/threaddata.cpp
@@ -4,14 +4,27 @@
 /*#########################################################################*/
 /*#########################################################################*/
 
+#ifdef _WIN32
+
+#include <windows.h>
+
+static void UnhandledExceptionTranslator( unsigned int exceptionCode , struct _EXCEPTION_POINTERS *exceptionInfo ) {
+	throw RuntimeException( exceptionCode , 3 , exceptionInfo -> ExceptionRecord -> ExceptionAddress );
+}
+
+#else
+#endif
+
 ThreadData::ThreadData() {
-	memset( &threadExtId , 0 , sizeof( RFC_THREAD ) );
-	threadId = ( unsigned long )NULL;
+	memset( &data , 0 , sizeof( RFC_THREADDATA ) );
+
 	object = NULL;
 	objectFunction = NULL;
 	objectFunctionArg = NULL;
 	sleepEvent = rfc_hnd_evcreate();
 	stopped = false;
+
+	data.userdata = this;
 }
 
 ThreadData::~ThreadData() {
@@ -25,4 +38,11 @@ void ThreadData::stopThread() {
 
 bool ThreadData::checkStopped() {
 	return( stopped );
+}
+
+void ThreadData::setHandleExceptions() {
+#ifdef _WIN32
+	oldhandler = _set_se_translator( UnhandledExceptionTranslator );
+#else
+#endif
 }
