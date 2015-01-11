6963b90b223c3ce461f0a44a50a514bba0cf7c13
vsavchik <vsavchik@gmail.com>
2015-01-11 10:27:05 +0000

    rework

--- externals/generic/src/modos/_gen_win.c
+++ externals/generic/src/modos/_gen_win.c
@@ -26,8 +26,7 @@ RFC_HND
 /* semaphore & event operations */
 
 /* lock semaphore */
-int rfc_hnd_semlock( RFC_HND p_hnd )
-{
+int rfc_hnd_semlock( RFC_HND p_hnd ) {
 	if( WaitForSingleObject( p_hnd , INFINITE ) != WAIT_OBJECT_0 )
 		return( -1 );
 
@@ -35,27 +34,23 @@ int rfc_hnd_semlock( RFC_HND p_hnd )
 }
 
 /* release semaphore */
-void rfc_hnd_semunlock( RFC_HND p_hnd )
-{
+void rfc_hnd_semunlock( RFC_HND p_hnd ) {
 	ReleaseSemaphore( p_hnd , 1 , NULL );
 }
 
 /* create semaphore */
-RFC_HND rfc_hnd_semcreate()
-{
+RFC_HND rfc_hnd_semcreate() {
 	return( CreateSemaphore( NULL , 1 , 1 , NULL ) );
 }
 
 /* destroy semaphore */
-void rfc_hnd_semdestroy( RFC_HND p_hnd )
-{
+void rfc_hnd_semdestroy( RFC_HND p_hnd ) {
 	CancelIoEx( p_hnd , NULL );
 	CloseHandle( p_hnd );
 }
 
 /* wait to event signaled */
-short rfc_hnd_waitevent( RFC_HND p_hnd , int p_timeout_ms )
-{
+short rfc_hnd_waitevent( RFC_HND p_hnd , int p_timeout_ms ) {
 	DWORD waitParam = p_timeout_ms;
 	if( p_timeout_ms < 0 )
 		waitParam = INFINITE;
@@ -66,52 +61,44 @@ short rfc_hnd_waitevent( RFC_HND p_hnd , int p_timeout_ms )
 }
 
 /* create event */
-RFC_HND rfc_hnd_evcreate()
-{
+RFC_HND rfc_hnd_evcreate() {
 	return( CreateEvent( NULL , TRUE , FALSE , NULL ) );
 }
 
 /* destroy event */
-void rfc_hnd_evdestroy( RFC_HND p_hnd )
-{
+void rfc_hnd_evdestroy( RFC_HND p_hnd ) {
 	CancelIoEx( p_hnd , NULL );
 	CloseHandle( p_hnd );
 }
 
 /* set signaled state to event */
-void rfc_hnd_evsignal( RFC_HND p_hnd )
-{
+void rfc_hnd_evsignal( RFC_HND p_hnd ) {
 	SetEvent( p_hnd );
 }
 
 /* clear signaled state of event */
-void rfc_hnd_evreset( RFC_HND p_hnd )
-{
+void rfc_hnd_evreset( RFC_HND p_hnd ) {
 	ResetEvent( p_hnd );
 }
 
 /* create mutex */
-RFC_HND		rfc_hnd_mutcreate( void )
-{
+RFC_HND		rfc_hnd_mutcreate( void ) {
 	return( CreateMutex( NULL , FALSE , NULL ) );
 }
 
 /* destroy mutex */
-void		rfc_hnd_mutdestroy( RFC_HND p_hnd )
-{
+void		rfc_hnd_mutdestroy( RFC_HND p_hnd ) {
 	CancelIoEx( p_hnd , NULL );
 	CloseHandle( p_hnd );
 }
 
 /* lock mutex */
-void		rfc_hnd_mutlock( RFC_HND p_hnd )
-{
+void		rfc_hnd_mutlock( RFC_HND p_hnd ) {
 	WaitForSingleObject( p_hnd , INFINITE );
 }
 
 /* unlock mutex */
-void		rfc_hnd_mutunlock( RFC_HND p_hnd )
-{
+void		rfc_hnd_mutunlock( RFC_HND p_hnd ) {
 	ReleaseMutex( p_hnd );
 }
 
@@ -129,27 +116,23 @@ static void UnhandledExceptionTranslator( unsigned int exceptionCode , struct _E
 	( *td -> exception_translator )( td , exceptionCode , exceptionInfo -> ExceptionRecord -> ExceptionAddress );
 }
 
-void rfc_thr_initthread( RFC_THREADDATA *rd )
-{
+void rfc_thr_initthread( RFC_THREADDATA *rd ) {
 	TlsSetValue( tlsIndex , rd );
 	rd -> threadId = GetCurrentThreadId();
 	rd -> oldhandler = _set_se_translator( UnhandledExceptionTranslator );
 }
 
-RFC_THREADDATA *rfc_thr_getdata()
-{
+RFC_THREADDATA *rfc_thr_getdata() {
 	return( ( RFC_THREADDATA * )TlsGetValue( tlsIndex ) );
 }
 
-void rfc_thr_exitthread()
-{
+void rfc_thr_exitthread() {
 	// to be no more used
 	TlsSetValue( tlsIndex , NULL );
 }
 
 /* wait to exit thread */
-int rfc_thr_waitexit( RFC_THREAD *p_hnd )
-{
+int rfc_thr_waitexit( RFC_THREAD *p_hnd ) {
 	if( WaitForSingleObject( p_hnd -> s_ih , INFINITE ) != WAIT_OBJECT_0 )
 		return( -1 );
 
@@ -157,14 +140,12 @@ int rfc_thr_waitexit( RFC_THREAD *p_hnd )
 }
 
 /* close thread handle */
-void rfc_thr_closehandle( RFC_THREAD *p_hnd )
-{
+void rfc_thr_closehandle( RFC_THREAD *p_hnd ) {
 	CloseHandle( p_hnd -> s_ih );
 }
 
 /* execution thread */
-int rfc_thr_process( RFC_THREAD *p_thread , void *p_arg , RFC_THRFUNC p_action )
-{
+int rfc_thr_process( RFC_THREAD *p_thread , void *p_arg , RFC_THRFUNC p_action ) {
 	p_thread -> s_ih = ( RFC_HND )_beginthreadex( NULL , 0 , p_action , p_arg , 1 , &p_thread -> s_ip );
 	if( p_thread -> s_ih == 0 )
 		return( -1 );
@@ -172,38 +153,51 @@ int rfc_thr_process( RFC_THREAD *p_thread , void *p_arg , RFC_THRFUNC p_action )
 }
 
 /* exit thread */
-void rfc_thr_threxit( int p_exitcode )
-{
+void rfc_thr_threxit( int p_exitcode ) {
 	_endthreadex( p_exitcode );
 }
 
 /* sleep current thread */
-void rfc_thr_sleep( int p_sec )
-{
+void rfc_thr_sleep( int p_sec ) {
 	Sleep( p_sec * 1000 );
 }
 
-int rfc_thr_increment( int *pv )
-{
+int rfc_thr_increment( int *pv ) {
 	return( ( int )InterlockedIncrement( ( LPLONG )pv ) );
+}
+
+void rfc_thr_register( RFC_THREADDATA *p_data ) {
+	p_data -> threadExtId.s_ih = OpenThread( THREAD_ALL_ACCESS , FALSE , GetCurrentThreadId() );
+	p_data -> threadExtId.s_ip = 0;
+}
+
+unsigned long rfc_thr_getcurrentid() {
+	return( GetCurrentThreadId() );
+}
+
+void rfc_thr_suspend( RFC_THREAD *p_hnd ) {
+	RFC_HND handle = p_hnd -> s_ih;
+	SuspendThread( handle );
+}
+
+void rfc_thr_resume( RFC_THREAD *p_hnd ) {
+	RFC_HND handle = p_hnd -> s_ih;
+	ResumeThread( handle );
 }
 
 /*#######################################################*/
 /*#######################################################*/
 /* library management */
 
-RFC_HND rfc_lib_load( const char *p_lib )
-{
+RFC_HND rfc_lib_load( const char *p_lib ) {
 	return( LoadLibrary( p_lib ) );
 }
 
-RFC_PROC rfc_lib_getfunc( RFC_HND p_lib , const char *p_func )
-{
+RFC_PROC rfc_lib_getfunc( RFC_HND p_lib , const char *p_func ) {
 	return( ( RFC_PROC )GetProcAddress( ( HINSTANCE )p_lib , p_func ) );
 }
 
-void rfc_lib_unload( RFC_HND p_lib )
-{
+void rfc_lib_unload( RFC_HND p_lib ) {
 	FreeLibrary( ( HINSTANCE )p_lib );
 }
 
@@ -211,8 +205,7 @@ void rfc_lib_unload( RFC_HND p_lib )
 /*#######################################################*/
 /* environment */
 
-void rfc_env_gethostname( char *p_name , int p_size )
-{
+void rfc_env_gethostname( char *p_name , int p_size ) {
 	GetComputerName( p_name , ( DWORD * )&p_size );
 }
 
@@ -220,9 +213,7 @@ void rfc_env_gethostname( char *p_name , int p_size )
 /*#######################################################*/
 
 /* translate from unicode using codepage */
-short
-	rfc_str_setcp( char *p_s , const unsigned short *p_v , int p_cp )
-{
+short rfc_str_setcp( char *p_s , const unsigned short *p_v , int p_cp ) {
 	int l_len;
 
 	l_len = rfc_wstr_len( p_v );
@@ -234,9 +225,7 @@ short
 }
 
 /* translate from ansi using codepage */
-short
-	rfc_wstr_setcp( unsigned short *p_s , const char *p_v , int p_cp )
-{
+short rfc_wstr_setcp( unsigned short *p_s , const char *p_v , int p_cp ) {
 	int l_len;
 
 	l_len = strlen( p_v );
@@ -256,16 +245,12 @@ static long hpt_start_clocks;
 static LONGLONG hpt_window_ticks;
 static LONGLONG hpt_window_clocks;
 
-void
-	rfc_hpt_startadjustment()
-{
+void rfc_hpt_startadjustment() {
 	QueryPerformanceCounter( &hpt_start_ticks );
 	hpt_start_clocks = GetTickCount();
 }
 
-void
-	rfc_hpt_stopadjustment()
-{
+void rfc_hpt_stopadjustment() {
 	LARGE_INTEGER hpt_stop_ticks;
 	long htp_stop_clocks;
 
@@ -276,38 +261,28 @@ void
 	hpt_window_clocks = htp_stop_clocks - hpt_start_clocks;
 }
 
-void
-	rfc_hpt_setpoint( RFC_INT64 *pv )
-{
+void rfc_hpt_setpoint( RFC_INT64 *pv ) {
 	QueryPerformanceCounter( ( LARGE_INTEGER * )pv );
 }
 
-RFC_INT64
-	rfc_hpt_timepassed( RFC_INT64 *pv )
-{
+RFC_INT64 rfc_hpt_timepassed( RFC_INT64 *pv ) {
 	LARGE_INTEGER hpt_stop_ticks;
 	QueryPerformanceCounter( &hpt_stop_ticks );
 	return( hpt_stop_ticks.QuadPart - *pv );
 }
 
-RFC_INT64
-	rfc_hpt_ticks2ms( RFC_INT64 ticks )
-{
+RFC_INT64 rfc_hpt_ticks2ms( RFC_INT64 ticks ) {
 	return( ( ticks * hpt_window_clocks ) / hpt_window_ticks );
 }
 
-RFC_INT64
-	rfc_hpt_ms2ticks( RFC_INT64 ms )
-{
+RFC_INT64 rfc_hpt_ms2ticks( RFC_INT64 ms ) {
 	return( ( ms * hpt_window_ticks ) / hpt_window_clocks );
 }
 
 /*#######################################################*/
 /*#######################################################*/
 
-float		
-	rfc_sys_getcpuload( CPULOADINFO *info )
-{
+float rfc_sys_getcpuload( CPULOADINFO *info ) {
 	double timeFree, timeBusy;
 	float cpuLoad;
 
@@ -364,9 +339,7 @@ float
 	return( cpuLoad );
 }
 
-short
-	rfc_sys_getfileinfo( const char *path , RFC_FILEINFO *info )
-{
+short rfc_sys_getfileinfo( const char *path , RFC_FILEINFO *info ) {
 	WIN32_FIND_DATA findFileData;
 	HANDLE hFind;
 	RFC_MONEY value;
